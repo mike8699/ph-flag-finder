317c019dc725c1e9522873c7ec63cc85c1647a9a
Mike <mikebuntu68@gmail.com>
2023-11-04 16:02:37 -0400

    Write flag data to disk

--- phff.py
+++ phff.py
@@ -1,6 +1,7 @@
+import json
+from dataclasses import asdict, dataclass
 from datetime import datetime
 from pathlib import Path
-
 from tkinter import filedialog
 
 import cv2
@@ -26,11 +27,19 @@ CONTROLS = {
 }
 
 PARENT_DIRECTORY = Path(f'output_{datetime.now().strftime("%Y%m%d%H%M%S")}')
-PARENT_DIRECTORY.mkdir()
 
 
-def write_frames_to_video(frames: list[Image.Image]) -> str:
-    filename = PARENT_DIRECTORY / f'{datetime.now().strftime("%Y%m%d%H%M%S")}.mp4'
+@dataclass
+class FlagSet:
+    base_addr: int
+    param2: int
+    set: bool
+    thumbnail: str
+    video: str
+
+
+def write_frames_to_video(frames: list[Image.Image], timestamp: str) -> str:
+    filename = PARENT_DIRECTORY / f"{timestamp}.mp4"
     video = cv2.VideoWriter(
         str(filename),
         cv2.VideoWriter_fourcc(*"avc1"),
@@ -39,18 +48,41 @@ def write_frames_to_video(frames: list[Image.Image]) -> str:
     )
     for img in frames:
         video.write(cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR))
+    return filename
 
 
 def main() -> None:
     file_path = filedialog.askopenfilename()
 
+    flags: list[FlagSet] = []
+
     emu = DeSmuME()
     emu.open(file_path)
+
+    PARENT_DIRECTORY.mkdir()
 
     video_frames: list[Image.Image] = []
 
     def set_flag_breakpoint(frames: list[Image.Image]) -> None:
-        write_frames_to_video(frames)
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        video_file = str(write_frames_to_video(frames, timestamp))
+        screenshot_file = str(PARENT_DIRECTORY / f"{timestamp}.png")
+        emu.screenshot().save(screenshot_file)
+        emu.savestate.save_file(str(PARENT_DIRECTORY / f"{timestamp}.dsv"))
+        (PARENT_DIRECTORY / f"{timestamp}.json").write_text(
+            json.dumps(
+                asdict(
+                    FlagSet(
+                        base_addr=emu.memory.register_arm9.r0,
+                        param2=emu.memory.register_arm9.r1,
+                        set=bool(emu.memory.register_arm9.r2),
+                        thumbnail=screenshot_file,
+                        video=video_file,
+                    )
+                ),
+                indent=2,
+            )
+        )
 
     emu.memory.register_exec(
         0x209773C, lambda addr, size: set_flag_breakpoint(video_frames)
@@ -61,7 +93,7 @@ def main() -> None:
 
     while not window.has_quit():
         video_frames.append(emu.screenshot())
-        if len(video_frames) > 5000:
+        if len(video_frames) > 600:
             video_frames = video_frames[1:]
 
         window.process_input()
