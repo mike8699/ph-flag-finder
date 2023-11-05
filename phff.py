0968fce75cfebde32252a562285fa6f2920384f6
Mike <mikebuntu68@gmail.com>
2023-11-04 21:23:19 -0400

    Document code a bit

--- phff.py
+++ phff.py
@@ -28,6 +28,7 @@ class FlagSet:
 
 
 def write_frames_to_video(frames: list[Image.Image], timestamp: str) -> str:
+    """Convert a list of images to a video and writes it to disk."""
     filename = PARENT_DIRECTORY / f"{timestamp}.mp4"
     video = cv2.VideoWriter(
         str(filename),
@@ -51,25 +52,37 @@ def main() -> None:
     video_frames: list[Image.Image] = []
 
     def set_flag_breakpoint(frames: list[Image.Image]) -> None:
+        # Get string timestamp to use in filenames
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        # Generate video
         video_file = str(write_frames_to_video(frames, timestamp))
+        # Generate screenshot
         screenshot_file = str(PARENT_DIRECTORY / f"{timestamp}.png")
         emu.screenshot().save(screenshot_file)
+        # Create save state
         emu.savestate.save_file(str(PARENT_DIRECTORY / f"{timestamp}.dsv"))
 
+        # Get the function arguments for the set flag function
         r0 = emu.memory.register_arm9.r0
         r1 = emu.memory.register_arm9.r1
         r2 = emu.memory.register_arm9.r2
 
+        # r0 contains the "base address" of the flags in memory
         base_address = r0
+        # Calculate the offset from the base address that the flag is located at
         flag_offset_from_base = (r1 >> 5) * 4
+        # Figure out what bit the flag is at
         flag_bit = 1 << (r1 & 0x1F)
+
+        # The game code gives these values in terms of words (i.e. 4 bytes),
+        # so to get it in terms of bytes, we need to do some conversion here
         while flag_bit > 0x80:
             flag_bit >>= 8
             flag_offset_from_base += 1
 
         flag_absolute_address = base_address + flag_offset_from_base
 
+        # r2 is a boolean, which determines whether the flag should be set or unset
         set = bool(r2)
 
         (PARENT_DIRECTORY / f"{timestamp}.json").write_text(
@@ -92,11 +105,14 @@ def main() -> None:
             )
         )
 
+    # Register a breakpoint at the beginning of the set flag function
+    # that calls the callback defined above
     emu.memory.register_exec(
         0x209773C, lambda addr, size: set_flag_breakpoint(video_frames)
     )
 
     while not emu.window.has_quit():
+        # Save current video frame and discard old ones
         video_frames.append(emu.screenshot())
         if len(video_frames) > 600:
             video_frames = video_frames[1:]
